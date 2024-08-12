3d1992078d83231264faec61a4b655fd07a8b87d
unknown <skillixx66>
2024-08-12 19:20:09 +0800

    Site updated: 2024-08-12 19:20:09

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1723043816022";
+const VERSION = "1723461603302";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
